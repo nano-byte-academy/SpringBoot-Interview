a1fa80daf63bd8c918089049f668eb71e1c8a220
prosenjit005 <nanobyte.academy.tech@gmail.com>
2025-08-05 19:29:05 +0530

    initial commit

---
+++ Basics/StartupEvents/src/test/java/com/demo/events/StartupEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
